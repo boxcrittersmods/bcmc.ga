1867ce43d0b3d93ec7c0a729b3a1fad27a7c1aac
Tumble <tumblegamer@gmail.com>
2020-05-14 11:27:48 +0100

    safari number fix

--- js/browser.js
+++ js/browser.js
@@ -12,7 +12,7 @@ var isFirefox = typeof InstallTrigger !== 'undefined' || agent.indexOf("Firefox"
 
 // Safari 3.0+ "[object HTMLElementConstructor]" 
 var isSafari = 
-/constructor/i.test(window.HTMLElement) |
+/constructor/i.test(window.HTMLElement) ||
 (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
 
 // Internet Explorer 6-11
