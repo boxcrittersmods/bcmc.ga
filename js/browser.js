64ad9526a48fde04ae22edf6083f4d26411b0554
SArpnt <36542932+SArpnt@users.noreply.github.com>
2020-05-17 18:45:52 -0400

    better browser.js

--- js/browser.js
+++ js/browser.js
@@ -1,82 +1,52 @@
 var agent = navigator.userAgent;
 
-var noneText =
-	'<span class="browser-none">This tool is not supported on your browser.</span>';
-
 var isIE =
 	/*@cc_on!@*/ false || !!document.documentMode || agent.indexOf("MSIE") > -1;
-var isBrowser = [
+var isBrowser = {
 	// Opera 8.0+
-	(!!window.opr && !!opr.addons) ||
-	!!window.opera ||
-	agent.indexOf("Opera") > -1 ||
-	agent.indexOf("OPR") > -1,
+	".browser-opera": (!!window.opr && !!opr.addons) ||
+		!!window.opera ||
+		agent.indexOf("Opera") > -1 ||
+		agent.indexOf("OPR") > -1,
 	// Firefox 1.0+
-	typeof InstallTrigger !== "undefined" || agent.indexOf("Firefox") > -1,
+	".browser-firefox": typeof InstallTrigger !== "undefined" || agent.indexOf("Firefox") > -1,
 	// Safari 3.0+ "[object HTMLElementConstructor]"
-	/constructor/i.test(window.HTMLElement) ||
-	(function (p) {
-		return p.toString() === "[object SafariRemoteNotification]";
-	})(
-		!window["safari"] ||
-		(typeof safari !== "undefined" && safari.pushNotification)
-	),
+	".browser-safari": /constructor/i.test(window.HTMLElement) ||
+		(function (p) {
+			return p.toString() === "[object SafariRemoteNotification]";
+		})(
+			!window["safari"] ||
+			(typeof safari !== "undefined" && safari.pushNotification)
+		),
 	// Internet Explorer 6-11
-	isIE,
+	".browser-ie": isIE,
 	// Edge 20+
-	(!isIE && !!window.StyleMedia) ||
-	agent.indexOf("Edge") > -1 ||
-	agent.indexOf("Edg") > -1,
+	".browser-edge": (!isIE && !!window.StyleMedia) ||
+		agent.indexOf("Edge") > -1 ||
+		agent.indexOf("Edg") > -1,
 	// Chrome 1 - 71
-	(!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) ||
-	agent.indexOf("Chrome") > -1,
+	".browser-chrome": (!!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime)) ||
+		agent.indexOf("Chrome") > -1,
 	// Blink engine detection
-	//(isChrome || isOpera) && !!window.CSS,
-	//IOS
-	/iPhone|iPad|iPod/i.test(agent),
-	//Androis
-	/Android/i.test(agent),
-	//Mobile
-	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent),
-];
+	//".browser-blink": (isChrome || isOpera) && !!window.CSS,
+	// IOS
+	".browser-ios": /iPhone|iPad|iPod/i.test(agent),
+	// Android
+	".browser-android": /Android/i.test(agent),
+	// Mobile
+	".browser-mobile": /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(agent),
+};
 
-var browserQuery = [
-	".browser-opera",
-	".browser-firefox",
-	".browser-safari",
-	".browser-ie",
-	".browser-edge",
-	".browser-chrome",
-	".browser-blink",
-	".browser-ios",
-	".browser-android",
-	".browser-mobile",
-];
-var noneQuery = ".browser-none";
+for (let i in isBrowser)
+	if (isBrowser[i])
+		$('head').append(`<style>${i}{display:initial}</style>`);
 
-$(".browser-list").each(function () {
+$(".modrow-buttons").each(function () {
 	var bItem = $(this);
-	var browserSupported = browserQuery.map((bQuery, i) => {
-		var item = bItem.children(bQuery);
-		return {
-			item,
-			supported: isBrowser[i] && item.length > 0,
-		};
-	});
-	var noneSupported = {
-		item: bItem.children(noneQuery),
-		supported: !browserSupported
-			.map((bs) => bs.supported)
-			.reduce((sum, item) => sum || item),
-	};
-
-	browserSupported.forEach((bs) => {
-		if (bs.supported) {
-			bs.item.attr("style", "display: inline");
+	for (let i in isBrowser) {
+		if (isBrowser[i] && bItem.children(i).length > 0) {
+			bItem.children(".browser-none").hide();
+			break;
 		}
-	});
-
-	if (!noneSupported.supported) {
-		noneSupported.item.hide();
 	}
 });
